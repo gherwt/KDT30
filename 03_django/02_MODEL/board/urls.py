0fb75d3da7f5538807c9314220e8a45b28abd075
Lee Sang Min <gherwt46@gmail.com>
2023-10-05 13:40:03 +0900

    111

--- 03_django/02_MODEL/board/urls.py
+++ 03_django/02_MODEL/board/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    paht()
+    path('new/', views.new),
+    path(' ', views.index),
+    path('create/', views.create),
+    path('<int:pk>/', views.detail),
+    path('edit/', views.edit),
+    path('update/', views.update),
+    path('delete/', views.delete),
 ]
+
--- 03_django/02_MODEL/board/views.py
+++ 03_django/02_MODEL/board/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def new(request):
+    pass
+    return render(request, 'board/new.html')
+
+def index(request):
+    pass
+    return render(request, 'board/.html')
+
+def create(request):
+    pass
+    return render(request, 'board/create.html')
+
+def detail(request, pk):
+    pass
+    return render(request, 'board/<int:pk>.html',{
+        'pk' : pk
+    })
+
+def edit(request):
+    pass
+    return render(request, 'board/edit.html')
+
+def update(request):
+    pass
+    return render(request, 'board/update.html')
+
+def delete(request):
+    pass
+    return render(request, 'board/delete.html')
