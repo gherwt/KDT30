c011302f5c86987992ada78976307b1035a07dac
Lee Sang Min <gherwt46@gmail.com>
2023-10-06 12:06:32 +0900

    lunch before

--- 03_django/02_MODEL/board/urls.py
+++ 03_django/02_MODEL/board/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.index),
     path('new/',views.new),
     path('create/',views.create),
-    path('',views.index),
     path('<int:pk>/',views.detail),
     path('<int:pk>/edit/',views.edit),
     path('<int:pk>/update/',views.update),
