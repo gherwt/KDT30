f89458ea91535dad646aa63ef98a7dded400d37a
Lee Sang Min <gherwt46@gmail.com>
2023-10-10 16:23:56 +0900

    form

--- 03_django/02_MODEL/board/forms.py
+++ 03_django/02_MODEL/board/forms.py
@@ -1,13 +1,16 @@
 # board/forms
 from django import forms
 from .models import Article
-# 1. 입력 데이터 검증 과정 X 
-# 2. HTML 만들기 귀찮다
 
+# 0. 조건이다. (fields에 적힌 컬럼에 대해서만) 
+# 1. 입력 데이터 검증 -> 저장
+# 2. HTML(input/textarea/....)을 생성
 class ArticleForm(forms.ModelForm):
-    # 0. 조건이다. (fields에 적힌 컬럼에 대해서만) 
-    # 1. 입력 데이터 검증 -> 저장
-    # 2. HTML(input/textarea/....)을 생성
+    
+    # django 에서 문자열을 검증할 필드
+    title = forms.CharField(min_length=2)
+    content = forms.CharField(min_length=5)
+
 
     class Meta:
         model = Article
--- 03_django/02_MODEL/board/models.py
+++ 03_django/02_MODEL/board/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+# 데이터 타입만 나타낸다.
 class Article(models.Model):
+    # SQL) VARCHAR
     title = models.CharField(max_length=200)
+    # SQL) TEXT
     content = models.TextField()
 
