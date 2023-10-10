0f385840647d3e0eaef84703486fe65085231fc5
Lee Sang Min <gherwt46@gmail.com>
2023-10-10 17:23:03 +0900

    today class

--- 03_django/02_MODEL/board/forms.py
+++ 03_django/02_MODEL/board/forms.py
@@ -6,10 +6,18 @@ from .models import Article
 # 1. 입력 데이터 검증 -> 저장
 # 2. HTML(input/textarea/....)을 생성
 class ArticleForm(forms.ModelForm):
-    
     # django 에서 문자열을 검증할 필드
-    title = forms.CharField(min_length=2)
-    content = forms.CharField(min_length=5)
+    # 검증식을 상세하게 설정해줄 수 있다.
+    title = forms.CharField(min_length=2, max_length=40)
+    content = forms.CharField(
+        min_length=5,
+        # fomr.charfield 는 기본값이 input type 이다.
+        # widget을 통해 HTML 코드도 바꿀 수 있다.
+        widget=forms.Textarea(
+            # class 주는 코드 예시
+            attrs={'class' : 'my-class'}
+        )
+    )
 
 
     class Meta:
--- 03_django/02_MODEL/board/views.py
+++ 03_django/02_MODEL/board/views.py
@@ -3,32 +3,47 @@ from django.shortcuts import render, redirect
 from .models import Article 
 from .forms import ArticleForm
 
-# Create
-def new(request):
-    form = ArticleForm() # input tag 를 대신 생성
-    return render(request, 'board/new.html', {
-        'form' : form,
-    }) 
 
-def create(request):
+# 요청 -> WHERE(=URL), HOW(=METHOD)
+# Where => /board/create/
+# How => POST, GET 의 차이로 같은 URL 에서 다른 동작을 할 수 있는 것임.
 
+def create(request): 
+    if request.method == 'GET':
+        form = ArticleForm() # input tag 를 대신 생성
+        return render(request, 'board/form.html', {
+                'form' : form,
+        }) 
+    
     # 데이터 입력
-    form = ArticleForm(data=request.POST)
-    
-    # 데이터 검증 
+    elif request.method == 'POST':
+        form = ArticleForm(data=request.POST)
+        # 데이터 검증 
+        # 유효한 데이터라면,
+        if form.is_valid():
+            # validation -> 유효성 검증// is_(T or F return)
+            # 저장
+            article = form.save()
+            return redirect('board:detail', article.pk)
+        
+        # 유효하지 않은 데이터라면,
+        else:
+            return render(request, 'board/form.html', {
+                'form' : form,
+            })
 
-    # 유효한 데이터라면,
-    if form.is_valid():
-        # validation -> 유효성 검증// is_(T or F return)
-        # 저장
-        article = form.save()
-        return redirect('board:detail', article.pk)
-    
-    # 유효하지 않은 데이터라면,
-    else:
-        return render(request, 'board/new.html', {
-            'form' : form,
-        })
+# 리펙토링 후
+# def create(request): 
+#     if request.method == 'GET':
+#         form = ArticleForm()
+#     elif request.method == 'POST':
+#         form = ArticleForm(data=request.POST)
+#         if form.is_valid():
+#             article = form.save()
+#             return redirect('board:detail', article.pk)
+#     return render(request, 'board/new.html', {
+#         'form' : form,
+#         })
  
 # read
 
@@ -45,39 +60,48 @@ def detail(request, pk):
         'article' : article,
     })
 
+
+# def update(request, pk):
+
+#     article = Article.objects.get(pk=pk)
+#     # 기존에 있는 data 를 가져온다.
+#     form = ArticleForm(data=request.POST, instance = article)
+    
+#     # 데이터 검증 
+
+#     # 유효한 데이터라면,
+#     if form.is_valid():
+#         # validation -> 유효성 검증// is_(T or F return)
+#         # 저장
+#         article = form.save()
+#         return redirect('board:detail', article.pk)
+    
+#     # 유효하지 않은 데이터라면,
+#     else:
+#         return render(request, 'board/form.html', {
+#             'article' : article,
+#             'form' : form,
+#         })
+    
 # update
 def edit(request, pk):
     article = Article.objects.get(pk=pk)
-    form = ArticleForm(instance = article)
+    
+    if request.method == 'GET':
+        form = ArticleForm(instance = article)
 
+    elif request.method == 'POST': 
+        form = ArticleForm(data=request.POST, instance = article) 
+        if form.is_valid():
+            article = form.save()
+            return redirect('board:detail', article.pk)
+        
     return render(request, 'board/edit.html', {
-        'article' : article,
+        # 'article' : article,
         'form' : form,
     })
-
-
-def update(request, pk):
-
-    article = Article.objects.get(pk=pk)
-    # 기존에 있는 data 를 가져온다.
-    form = ArticleForm(data=request.POST, instance = article)
-    
-    # 데이터 검증 
-
-    # 유효한 데이터라면,
-    if form.is_valid():
-        # validation -> 유효성 검증// is_(T or F return)
-        # 저장
-        article = form.save()
-        return redirect('board:detail', article.pk)
-    
-    # 유효하지 않은 데이터라면,
-    else:
-        return render(request, 'board/edit.html', {
-            'article' : article,
-            'form' : form,
-        })
- 
+# url 에서 update 를 지우고 edit 만 살리고
+# detail 에서 update 를 edit 으로
 
 # delete
 def delete(request, pk):
