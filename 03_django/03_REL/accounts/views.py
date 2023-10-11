4d696ff05fc8b6efb0169a813711b732d6c49286
Lee Sang Min <gherwt46@gmail.com>
2023-10-11 17:23:31 +0900

    today class

--- 03_django/03_REL/accounts/views.py
+++ 03_django/03_REL/accounts/views.py
@@ -8,21 +8,30 @@ from django.contrib.auth import login as auth_login, logout as auth_logout
 # 계정 생성 -> 권한을 부여한다.
 @require_http_methods(['GET', 'POST'])
 def signup(request):
-    print(request.user)
+    # 요청 보낸 사용자가 인증되었다면 => 바로 돌려보내기
+    if request.user.is_authenticated:
+        return redirect('board:index')
     if request.method == 'POST':
         form = UserCreationForm(data = request.POST)
         if form.is_valid():
-            form.save()
+            # 회원 가입 후 정보를 저장해준다. 
+            user = form.save()
+            auth_login(request, user)
             return redirect('board:index')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html',{
         'form' : form,
     })
+
 @require_http_methods(['GET', 'POST'])
 def login(request):
+    # 요청 보낸 사용자가 인증되었다면 => 바로 돌려보내기
+    if request.user.is_authenticated:
+        return redirect('board:index')
     if request.method == 'POST':
         # id, password 검증해주는 것
+        # 사용자 특정짓기
         form = AuthenticationForm(request, data = request.POST)
         if form.is_valid():
             user = form.get_user()
