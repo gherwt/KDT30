f0c26e606ac222d6b0087fda42fabc4676dbf7b3
Lee Sang Min <gherwt46@gmail.com>
2023-10-11 09:59:04 +0900

    django

--- 03_django/02_MODEL/board/views.py
+++ 03_django/02_MODEL/board/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_POST, require_http_methods
 
 from .models import Article 
 from .forms import ArticleForm
@@ -33,20 +34,24 @@ from .forms import ArticleForm
 #             })
 
 # 리펙토링 후
+
+@require_http_methods(['GET', 'POST'])
 def create(request): 
     if request.method == 'GET':
         form = ArticleForm()
+
     elif request.method == 'POST':
         form = ArticleForm(data=request.POST)
         if form.is_valid():
             article = form.save()
             return redirect('board:detail', article.pk)
+        
     return render(request, 'board/form.html', {
         'form' : form,
         })
  
 # read
-
+@require_safe
 def index(request):
     # 모든 게시물 조회
     articles = Article.objects.all()
@@ -54,8 +59,10 @@ def index(request):
         'articles' : articles,
     })
 
+@require_safe
 def detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    # article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     return render(request, 'board/detail.html', {
         'article' : article,
     })
@@ -84,8 +91,9 @@ def detail(request, pk):
 #         })
     
 # update
+@require_http_methods(['GET', 'POST'])
 def edit(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
 
     if request.method == 'GET':
         form = ArticleForm(instance = article)
@@ -103,8 +111,13 @@ def edit(request, pk):
 # url 에서 update 를 지우고 edit 만 살리고
 # detail 에서 update 를 edit 으로
 
-# delete
+# delete - POST 요청을 보내야한다. DB에 영향을 미치기 때문임.
+
+@require_POST
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
     article.delete()
     return redirect('board:index')
+
+# URL에서 Delete 를 입력하면 GET 방식으로 지워진다.
+# 따로 설정해줘야 Delete 가 발생하지 않는다.
